d9065250da7c85a676340a3b8755e77166b1066c
camilo.a.navas <camilo.a.navas@95ff96ea-1fd9-11df-9829-b54b764236af>
2010-03-07 02:52:47 +0000

    -

--- trunk/Lab3Chat/src/Servidor/ClaseMain.java
+++ trunk/Lab3Chat/src/Servidor/ClaseMain.java
@@ -1,9 +1,8 @@
 package Servidor;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 
 /**
  * Clase main.
